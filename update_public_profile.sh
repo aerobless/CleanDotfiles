738d58c2a20f771f4a0a3f011c46965049620228
TheoWinter <theo.winter@gmail.com>
2014-04-09 09:54:33 +0200

    added rename to .sh
    so that hopefully github will correctly format the file.

--- update_public_profile.sh
+++ update_public_profile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Updating the public_profile to git"
 cp /Users/theowinter/Dropbox/SYNC/public_profile /Users/theowinter/git/UnixShell/
+mv /Users/theowinter/git/UnixShell/public_profile public_profile.sh
