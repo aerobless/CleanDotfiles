e89cf6799cd2d11dd996352d901bdc4568d751d7
aerobless <theo.winter@gmail.com>
2014-04-26 10:45:50 +0200

    added lines of code utility alias and ability to run a screensaver on the desktop

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,10 +1,11 @@
 #	-------------------------------------
 #			TERMINAL IMPROVEMENTS
 #	-------------------------------------
-alias finder='open -a "Path Finder" ./'		# Open directory in finder. Replace "Path Finder" with "Finder" if you don't own Path Finder.
-alias numFiles='echo $(ls -1 | wc -l)'		# Show the number of visible files in folder.
-alias reload_profile=". ~/.bash_profile"	# Reload the profiles
-alias preview='open -a "Preview"'			# Open file in preview
+alias finder='open -a "Path Finder" ./'			# Open directory in finder. Replace "Path Finder" with "Finder" if you don't own Path Finder.
+alias numFiles='echo $(ls -1 | wc -l)'			# Show the number of visible files in folder.
+alias reload_profile=". ~/.bash_profile"		# Reload the profiles
+alias preview='open -a "Preview"'				# Open file in preview
+alias loc='git ls-files | xargs cat | wc -l'	# Lines of code in a local git-repository
 
 #ToxicTodo
 alias todo="java -jar /Users/theowinter/Dropbox/SYNC/apps/ToxicTodo/ToxicTodoClient.jar"
@@ -20,6 +21,8 @@ alias shadow_drive="hdiutil attach '/Users/theowinter/Dropbox/0_blood_system/sha
 
 #Web Quick Access
 alias music="open https://play.google.com/music/listen?view=/settings&hl=en&u=0#/now"
+
+alias screensaverDesktop='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background'
 
 #Mounts
 alias hsr_skripte="mount -t smbfs //twinter@c206.hsr.ch/skripte 'HSR Skripte'"
